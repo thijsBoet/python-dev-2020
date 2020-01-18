023ca21db8e9b73acb35dfdbcbd4041b5f287148
Matthijs Boet <m.boet@chello.nl>
2020-01-18 09:19:30 +0100

    first commit

---
+++ 3-basics/data-types.py
@@ -0,0 +1,46 @@
+import math
+
+# Fundamental Data Types
+int 
+print(type(2 + 4))
+print(type(2 - 4))
+
+float
+print(type(2 / 4))      # 0.5
+print(type(8.9 + 1.1))  # 10.0
+
+print(2 ** 3)
+print(5 // 4)
+print(6 % 4)
+
+# math functions
+print(round(3.1))
+print(abs(-20))
+print(math.ceil(3.1))
+print(math.factorial(10))
+print(math.pi)
+
+# operator precedence
+
+# ()
+# **
+# * /
+# + -
+
+print(bin(5))           # convert to binary value
+print(int('0b101', 2))  # convert back to decimal value
+
+str
+
+
+bool
+list
+tuple
+set
+dict
+
+# Classes -> custom types
+
+# Specialized Data Types -> Modules
+
+None
