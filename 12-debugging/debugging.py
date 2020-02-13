d28161fe1d440359f0d010ea28d690e631efa217
Matthijs Boet <m.boet@chello.nl>
2020-02-13 16:49:33 +0100

    started file IO section 13

---
+++ 12-debugging/debugging.py
@@ -0,0 +1,16 @@
+# debugging
+
+# linter
+# IDE/editor
+# read error Types/Messages well
+# python debugger => pdb
+import pdb
+
+def add(num1, num2):
+    pdb.set_trace()
+    return num1 + num2
+
+add(4, 'kjlhlkjh')
+# type help for list of commands in debugger cli
+# type help and command name for description
+# 
---
+++ 13-file-IO/file-IO.py
@@ -0,0 +1,5 @@
+# IO == input/output
+# like input image => output compressed image
+
+my_file = open('test.txt')
+print(my_file)
