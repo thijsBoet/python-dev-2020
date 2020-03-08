5a1e5ee1ecb4793f31c04f7624290761d8186070
Matthijs Boet <m.boet@chello.nl>
2020-03-08 16:04:35 +0100

    started web scraping

---
+++ 18-web-scraping/web-scraping.py
@@ -0,0 +1,4 @@
+from bs4 import BeautifulSoup
+
+# read domain.com/robots.txt for 'allowed' scraping for different user agents (* is others)
+# crawl-delay is second delay to prevent slow loading website
