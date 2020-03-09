f95d2bdf488191b123564ce1db69b768edc9854b
Matthijs Boet <m.boet@chello.nl>
2020-03-09 23:54:28 +0100

    worinking in Flask section 19 webdev

--- 19-web-development/server.py
+++ 19-web-development/server.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Matthijs!'
+
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('./index.htm', name=username, post_id=post_id)
+
+
+@app.route('/about')
+def about():
+    return render_template('./about.htm')
+
+
+@app.route('/blog')
+def blog():
+    return 'These are my thoughts on web development in Python'
+
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'This is my dog'
+
 
 # export FLASK_APP=server.py                            set python file
 # export FLASK_ENV=development                          reset to developent mode so server restarts on change
