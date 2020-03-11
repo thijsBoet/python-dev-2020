3873932ea692218f36b7626c3161fd6a991552d8
Matthijs Boet <m.boet@chello.nl>
2020-03-11 03:28:20 +0100

    added original portfolio

---
+++ 19-web-development/3-portfolio2/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('./index.html')
+
+
+@app.route('/works')
+def works():
+    return render_template('./works.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('./about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('./contact.html')
+
+
+@app.route('/components')
+def components():
+    return render_template('./components.html')
+
+
+# export FLASK_APP=server.py                            set python file
+# export FLASK_ENV=development                          reset to developent mode so server restarts on change
+# flask run                                             run flask application
