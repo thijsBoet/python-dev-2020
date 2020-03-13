a8c8e458e3f98c79b79cd2826287c090a3daf15a
Matthijs Boet <m.boet@chello.nl>
2020-03-13 17:04:03 +0100

    build contact route section 19-3

--- 19-web-development/3-portfolio2/server.py
+++ 19-web-development/3-portfolio2/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
 
 
@@ -11,12 +12,32 @@ def home():
 #     return render_template(page_name)
 
 
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f'\n{email}, {subject}, {message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database,
+                                delimiter=',',
+                                quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
-        return redirect('./thankyou.html')
+        write_to_csv(data)
+        return render_template('./thankyou.html')
     else:
         return 'Something went wrong try again'
 
