2cb85156b55b89562229032152a7652e3a469fc4
Matthijs Boet <m.boet@chello.nl>
2020-03-14 16:19:42 +0100

    started 21 machine learning\

---
+++ 21-machine-learning/1-machine-learning.py
@@ -0,0 +1,6 @@
+# Data Science      == Overlaps with AI, Machine learning and Deep learning
+# --------------------------------------------------------------------------------------
+# AI -> Machine Learning -> Deep Learning
+# --------------------------------------------------------------------------------------
+# Machine Learning  == Achieve AI by systems that can recognize patters in a set of Data
+# Deep Learning     == One implementation of machine learning using neural networks
