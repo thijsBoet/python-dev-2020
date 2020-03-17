f6428a8b3eb7feb5092046de2ac0fc3bfda5b6ca
Matthijs Boet <m.boet@chello.nl>
2020-03-18 00:18:07 +0100

    finished course

--- 21-machine-learning/4-image-recog.py
+++ 21-machine-learning/4-image-recog.py
@@ -1,12 +1,19 @@
 from imageai.Prediction import ImagePrediction
 import os
-execution_path=os.getcwd()
+execution_path = os.getcwd()
 
 prediction = ImagePrediction()
-prediction.setModelTypeAsSqueezeNet()
-prediction.setModelPath(os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
+prediction.setModelTypeAsResNet()
+prediction.setModelPath(os.path.join(
+    execution_path,
+    # "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"
+    "resnet50_weights_tf_dim_ordering_tf_kernels.h5"
+    # "inception_v3_weights_tf_dim_ordering_tf_kernels.h5"
+    # "DenseNet-BC-121-32.h5"
+))
 prediction.loadModel()
 
-predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5 )
+predictions, probabilities = prediction.predictImage(
+    os.path.join(execution_path, "godzilla.jpg"), result_count=5)
 for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction , " : " , eachProbability)
+    print(eachPrediction, " : ", eachProbability)
